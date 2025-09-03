822ec1f43fe3909cdcefe5d26fdc58200b5ed787
Devingle <amitghanata01@gmail.com>
2025-09-04 00:38:02 +0530

    Adding Backend

---
+++ server/server.js
@@ -0,0 +1,36 @@
+/*Starts the server.
+
+Loads middleware (CORS, cookieParser, express.json).
+
+Connects MongoDB (from config/db.js).
+
+Uses routes from routes.*/
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.get("/", (req, res) => res.send("Backend running 🚀"));
+app.use("/auth", authRoutes);
+
+// Start server
+app.listen(5000, () => console.log("Server running at http://localhost:5000"));
