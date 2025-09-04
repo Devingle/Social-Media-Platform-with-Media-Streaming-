374367f421475905cb2a2bb6c4fe08212dee7132
Devingle <amitghanata01@gmail.com>
2025-09-04 17:47:44 +0530

    F/R passwords

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,15 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    verified: { type: Boolean, default: false },
+    verificationCode: { type: String },
+    // ✅ Forgot password fields
+    resetCode: String,
+    resetCodeExpires: Date,
+
+    // ✅ New fields for resend limits
+    lastResendAt: Date,
+    resendCount: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
