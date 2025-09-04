d4ad320d242849cdae09e9d68069d04ffbb20182
Devingle <amitghanata01@gmail.com>
2025-09-04 17:51:44 +0530

    add routes

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -12,4 +12,12 @@ export const login = (data) =>
   api.post("/auth/login", data).then((res) => res.data);
 export const getProfile = () =>
   api.get("/auth/profile").then((res) => res.data);
+export const verifyEmail = (data) =>
+  api.post("/auth/verify", data).then((res) => res.data);
+export const forgotPassword = (data) =>
+  api.post("/auth/forgot-password", data).then((res) => res.data);
+export const resetPassword = (data) =>
+  api.post("/auth/reset-password", data).then((res) => res.data);
+export const resendCode = (data) =>
+  api.post("/auth/resend-code", data).then((res) => res.data);
 export const logout = () => api.post("/auth/logout").then((res) => res.data);
