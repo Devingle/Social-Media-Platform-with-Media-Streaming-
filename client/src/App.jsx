2127a577dadeca260ab497ad847bef1ed01cee85
Devingle <amitghanata01@gmail.com>
2025-09-04 17:51:13 +0530

    Update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Landing, Membership, ProtectedRoute } from "./components";
 import Profile from "./pages/Profile";
+import VerifyEmail from "./pages/VerifyEmail";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 export default function App() {
   return (
@@ -10,6 +14,17 @@ export default function App() {
 
       {/* Membership page: accessible without login */}
       <Route path="/membership" element={<Membership />} />
+
+      <Route
+        path="/verify-email"
+        element={
+          <ErrorBoundary>
+            <VerifyEmail />
+          </ErrorBoundary>
+        }
+      />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
       {/* Profile page: protected */}
       <Route
